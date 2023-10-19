3fdee9664edcd0f566ab2bee17efb03107f439ef
bipularyal <59837845+bipularyal@users.noreply.github.com>
2023-10-19 12:06:48 -0500

    updated listing

--- src/components/listing/listing.component.jsx
+++ src/components/listing/listing.component.jsx
@@ -2,10 +2,15 @@ import "./listing.styles.scss";
 
 import Card from "../card/card.component";
 
-const Listing = () => (
-  <div className="directory">
-    <Card />
-  </div>
-);
+const Listing = () => {
+	const items = [1, 2, 3, 4, 5];
+	return (
+		<div className='directory'>
+			{items.map((item) => (
+				<Card />
+			))}
+		</div>
+	);
+};
 
 export default Listing;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,26 +3,29 @@ import searchBtn from "../../assets/search-btn.png";
 import Listing from "../../components/listing/listing.component";
 
 const Homepage = () => (
-  <div className="homepage">
-    <section className="homepage__hero">
-      <div className="homepage__hero-content">
-        <h2 className="homepage__hero-content--headline">
-          Renting made easier <br /> than ever before.
-        </h2>
-        <div className="homepage__hero-content--search-bar">
-          <input className="input" type="text" />
-          <img src={searchBtn} alt="" />
-        </div>
-      </div>
-    </section>
+	<div className='homepage'>
+		<section className='homepage__hero'>
+			<div className='homepage__hero-content'>
+				<h2 className='homepage__hero-content--headline'>
+					Renting made easier <br /> than ever before.
+				</h2>
+				<div className='homepage__hero-content--search-bar'>
+					<input className='input' type='text' />
+					<img src={searchBtn} alt='' />
+				</div>
+			</div>
+		</section>
 
-    <section className="homepage__featured-rentals">
-      <h2 className="section-title">Featured Rentals</h2>
-      <Listing />
-    </section>
+		<section className='homepage__featured-rentals'>
+			<h2 className='section-title'>Featured Rentals</h2>
+			<Listing />
+		</section>
 
-    <section className="homepage__featured-sales">FEATURED SALES</section>
-  </div>
+		<section className='homepage__featured-sales'>
+			<h2 className='section-title'>Featured Sales</h2>
+			<Listing />
+		</section>
+	</div>
 );
 
 export default Homepage;
