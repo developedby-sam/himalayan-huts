dbc2989ad0c4fd0a34209d68eca8d8f62e0c4b37
developedby-sam <developedbysam@gmail.com>
2023-10-18 23:26:22 -0500

    implemented navigation using react-router-dom and created hero section of the landing page

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,19 @@
+import "./homepage.styles.scss";
+import searchBtn from "../../assets/search-btn.png";
+
 const Homepage = () => (
   <div className="homepage">
-    <section className="homepage__hero"> HERO</section>
+    <section className="homepage__hero">
+      <div className="homepage__hero-content">
+        <h2 className="homepage__hero-content--headline">
+          Renting made easier <br /> than ever before.
+        </h2>
+        <div className="homepage__hero-content--search-bar">
+          <input className="input" type="text" />
+          <img src={searchBtn} alt="" />
+        </div>
+      </div>
+    </section>
     <section className="homepage__featured-rentals">FEATURED RENTALS</section>
     <section className="homepage__featured-sales">FEATURED SALES</section>
   </div>
